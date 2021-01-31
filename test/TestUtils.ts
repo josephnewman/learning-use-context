290fcb6028693859c9c48afeee922d1006a80f56
Joe Newman <joe.newman@outlook.com>
2021-01-31 15:53:12 +0000

    Convert forgotten files to TS

--- test/TestUtils.ts
+++ test/TestUtils.ts
@@ -1,6 +1,10 @@
 import * as ShoppingListContext from '../src/context/ShoppingListContext';
 
-const mockShoppingListContext = (value) => {
+interface MockShoppingList {
+  setShoppingListMock: () => {}
+}
+
+const mockShoppingListContext = (value: string[]): MockShoppingList => {
   const setShoppingListMock = jest.fn();
 
   jest.spyOn(ShoppingListContext, 'useShoppingListContext').mockImplementation(() => ({
